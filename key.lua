2ce63f5e2e5c9bfff907673dab9f6c2e45d2d046
ryanryan3113112-coder <ryanryan3113112@gmail.com>
2026-05-26 23:06:48 +0800

    Update key.lua

--- key.lua
+++ key.lua
@@ -1,1 +1,31 @@
 
+local CorrectKey = "MARTINET-2026"
+
+local key = getgenv().script_key
+
+if not key then
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Key System",
+        Text = "No Key Detected",
+        Duration = 5
+    })
+
+    return
+end
+
+if key ~= CorrectKey then
+    game:GetService("StarterGui"):SetCore("SendNotification",{
+        Title = "Key System",
+        Text = "Invalid Key",
+        Duration = 5
+    })
+
+    return
+end
+
+game:GetService("StarterGui"):SetCore("SendNotification",{
+    Title = "Key System",
+    Text = "Key Accepted",
+    Duration = 3
+})
+
